ad83d2111fa1b60bec610df579212975894e717d
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-04-22 10:28:33 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package com.vedansh;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int arr[] = {0,1,0,1,1,2,2,3,4,3,5};
+        var all = new HashSet<Integer>();
+        var duplicate = new HashSet<Integer>();
+        for(int ele: arr) {
+            if(all.contains(ele))
+                duplicate.add(ele);
+            else
+                all.add(ele);
+        }
+        all.removeAll(duplicate);
+        System.out.println(all);
+    }
+}
