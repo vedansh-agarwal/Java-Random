6d9ea1f91559c8629ffded3b78d55e58de915193
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-04-22 10:46:28 +0530

    Add files via upload

---
+++ InterleaveTwoLinkedList.java
@@ -0,0 +1,50 @@
+package com.vedansh;
+
+class LinkedList {
+    int val;
+    LinkedList next;
+}
+
+public class InterleaveTwoLinkedList {
+    public static void main(String[] args) {
+        LinkedList list1 = new LinkedList();
+        LinkedList list2 = new LinkedList();
+        LinkedList list1copy = list1;
+        LinkedList list2copy = list2;
+
+
+        for(int i = 1; i < 3; i++) {
+            list1copy.val = i;
+            list1copy.next = new LinkedList();
+            list1copy = list1copy.next;
+        }
+
+        for(int i = 4; i < 8; i++) {
+            list2copy.val = i;
+            list2copy.next = new LinkedList();
+            list2copy = list2copy.next;
+        }
+
+        LinkedList temp;
+        int counter = 0;
+        while(list1.next != null) {
+            if(counter%2 == 0) {
+                temp = list1.next;
+                list1.next = new LinkedList();
+                list1.next.val = list2.val;
+                list1.next.next = temp;
+                list2 = list2.next;
+            }
+            System.out.print(list1.val+" ");
+            list1 = list1.next;
+            counter++;
+        }
+
+        System.out.println();
+        while(list2.next != null) {
+            System.out.print(list2.val +" ");
+            list2 = list2.next;
+        }
+
+    }
+}
